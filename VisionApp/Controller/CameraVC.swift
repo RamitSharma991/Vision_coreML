8b4b670f017925542a705b330ca0972d755947f2
RamitSharma991 <RamitsMac@ramits-mbp.fritz.box>
2018-08-08 13:04:08 +0530

    Added AV Foundation, AV capture Sessions for using the camera

--- VisionApp/Controller/CameraVC.swift
+++ VisionApp/Controller/CameraVC.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
 //Outlets
+    
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     
     
@@ -23,8 +28,57 @@ class CameraVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) == true {
+                captureSession.canAddOutput(cameraOutput!)
+                
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession.startRunning()
+            }
+        } catch  {
+            debugPrint(error)
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
